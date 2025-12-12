39d7d6e5243a5c9bac48218e0589da1c58700ba6
Lareta064 <lareta1@yandex.ru>
2025-12-12 13:59:35 +0400

    promo-desk

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,49 @@
-//for-sub-menuposition
-function updateSubmenuPosition() {
-  const header = document.querySelector('.header');
-  const nav = document.querySelector('.navbar-nav');
-  const wrappers = document.querySelectorAll('.sub-menu-wrapper');
+ document.addEventListener('DOMContentLoaded', function () {
+    //for-sub-menuposition
+    function updateSubmenuPosition() {
+      const header = document.querySelector('.header');
+      const nav = document.querySelector('.navbar-nav');
+      const wrappers = document.querySelectorAll('.sub-menu-wrapper');
 
-  if (!header || !nav || !wrappers.length) return;
+      if (!header || !nav || !wrappers.length) return;
 
-  const navRect = nav.getBoundingClientRect();
-  const headerRect = header.getBoundingClientRect();
-  const vw = document.documentElement.clientWidth;
+      const navRect = nav.getBoundingClientRect();
+      const headerRect = header.getBoundingClientRect();
+      const vw = document.documentElement.clientWidth;
 
-  wrappers.forEach(wrapper => {
-    wrapper.style.top = headerRect.bottom + 'px';
+      wrappers.forEach(wrapper => {
+        wrapper.style.top = headerRect.bottom + 'px';
 
-    const leftPadding = navRect.left;
-    const rightPadding = vw - (navRect.right + 100);
+        const leftPadding = navRect.left;
+        const rightPadding = vw - (navRect.right + 100);
 
-    wrapper.style.paddingLeft = leftPadding + 'px';
-    wrapper.style.paddingRight = rightPadding + 'px';
-  });
-}
+        wrapper.style.paddingLeft = leftPadding + 'px';
+        wrapper.style.paddingRight = rightPadding + 'px';
+      });
+    }
+    window.addEventListener('DOMContentLoaded', () => {
+      updateSubmenuPosition();
+      requestAnimationFrame(updateSubmenuPosition);
+    });
 
-// 1) DOM собран
-window.addEventListener('DOMContentLoaded', () => {
-  updateSubmenuPosition();
-  // маленький хак: ещё один проход после отрисовки
-  requestAnimationFrame(updateSubmenuPosition);
+    window.addEventListener('load', updateSubmenuPosition);
+
+    window.addEventListener('resize', updateSubmenuPosition);
+    
+    //swipers
+    let promoSwiper = new Swiper('.promo-swiper',{
+      spaceBetween:16,
+      speed:1300,
+       loop:true,
+       navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+       autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    })
+
+ 
 });
-
-// 2) всё полностью загружено (картинки, шрифты и т.п.)
-window.addEventListener('load', updateSubmenuPosition);
-
-// 3) при ресайзе — как у тебя было
-window.addEventListener('resize', updateSubmenuPosition);
-
